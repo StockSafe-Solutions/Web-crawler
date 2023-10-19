7c81d8198d79a4288d54fff006a8d4a72d31a0b8
Julia Justino <stephany.justino@sptech.school>
2023-10-19 10:48:09 -0300

    feat(): Cria um JSON com o dataset geral

--- analiseDados/dadosR.R
+++ analiseDados/dadosR.R
@@ -1,5 +1,23 @@
+#===============================Importação============================
+library(jsonlite)
+
+
+
+
 summary(dfProduto$semana0)
-dfSemana0<-data.frame("Produto"=dfProduto$semana0, "Preco"=dfFonecedor$precoFonecedor0)
+#dfFornecedor$precoFonecedor0 = dfFornecedor$precoFonecedor0/100
+dfSemana0<-data.frame("Produto"=dfProduto$semana0, "Preco"=round(dfFornecedor$precoFonecedor0))
 
 #==============================Gráficos================================================= 
 hist(dfSemana0$Produto,  col="#abf5bf",main="Produtos vendidos", ylab = "Frequência", xlab = "Quantidade")
+hist(dfSemana0$Preco, col="#31572C", main="Preço do produto", ylab = "Frequência", xlab = "Valores")
+
+
+
+
+#==============================================Exportação de dataset===================================
+# Exporte o dataset para um JSON
+json_data <- toJSON(dfSemana0)
+caminho<-"~/Downloads/RGrupo/Web-crawler/analiseDados/teste.json"
+write_json(json_data, path=caminho)
+
